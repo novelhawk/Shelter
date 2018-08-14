d321e073311809269e5fd25f625ac487e2007d9b
Hawk of the Death <italia195@outlook.it>
2018-08-14 06:23:26 +0200

    Removed unused UI class.

--- MapNameChange.cs
+++ MapNameChange.cs
@@ -4,21 +4,18 @@ public class MapNameChange : MonoBehaviour
 {
     private void OnSelectionChange()
     {
-        LevelInfo info = LevelInfoManager.GetInfo(GetComponent<UIPopupList>().selection);
-        if (info != null)
-        {
-            GameObject.Find("LabelLevelInfo").GetComponent<UILabel>().text = info.Description;
-        }
-        if (!GetComponent<UIPopupList>().items.Contains("Custom"))
-        {
-            GetComponent<UIPopupList>().items.Add("Custom");
-            UIPopupList component = GetComponent<UIPopupList>();
-            component.textScale *= 0.8f;
-        }
-        if (!GetComponent<UIPopupList>().items.Contains("Custom (No PT)"))
-        {
-            GetComponent<UIPopupList>().items.Add("Custom (No PT)");
-        }
+//        LevelInfo info = LevelInfoManager.GetInfo(GetComponent<UIPopupList>().selection);
+//        GameObject.Find("LabelLevelInfo").GetComponent<UILabel>().text = info.Description;
+//        if (!GetComponent<UIPopupList>().items.Contains("Custom"))
+//        {
+//            GetComponent<UIPopupList>().items.Add("Custom");
+//            UIPopupList component = GetComponent<UIPopupList>();
+//            component.textScale *= 0.8f;
+//        }
+//        if (!GetComponent<UIPopupList>().items.Contains("Custom (No PT)"))
+//        {
+//            GetComponent<UIPopupList>().items.Add("Custom (No PT)");
+//        }
     }
 }
 
