1e7b48fb9d0484ca17525c76e9ba2cbf27e8cc3c
Hawk of the Death <italia195@outlook.it>
2018-08-15 16:30:51 +0200

    Made LocalOnly compute every call

--- Mod/Interface/Components/ChatMessage.cs
+++ Mod/Interface/Components/ChatMessage.cs
@@ -1,18 +1,17 @@
-﻿namespace Mod
+﻿namespace Mod.Interface.Components
 {
     public struct ChatMessage
     {
         public string Message { get; }
         public Player Sender { get; }
         public long Time { get; }
-        public bool LocalOnly { get; }
+        public bool LocalOnly => Sender == null;
 
         public ChatMessage(object message, Player sender = null)
         {
             Message = message.ToString();
             Sender = sender;
             Time = Shelter.Stopwatch.ElapsedMilliseconds;
-            LocalOnly = sender == null;
         }
     }
 }
