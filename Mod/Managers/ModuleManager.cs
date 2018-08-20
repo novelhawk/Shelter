637cf5ef7e30fd724aa35d3faa9f0dbd4480d726
mHawk <italia195@outlook.it>
2018-08-21 01:02:00 +0200

    Added Modules system

--- Mod/Managers/ModuleManager.cs
+++ Mod/Managers/ModuleManager.cs
@@ -1,7 +1,44 @@
-﻿namespace Mod.Managers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Mod.Managers
 {
-    public class ModuleManager
+    public sealed class ModuleManager
     {
+        private static readonly List<Module> _modules = new List<Module>();
         
+        public ModuleManager()
+        {
+            GameObject obj = new GameObject("Modules");
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (!assembly.FullName.Contains("Assembly-CSharp, ")) //Assembly-CSharp, Version=3.6.2.0, Culture=neutral, PublicKeyToken=null
+                    continue;
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (type.Namespace != "Mod.Modules")
+                        continue;
+
+                    if (obj.AddComponent(type) is Module module)
+                        _modules.Add(module);
+                }
+            }
+            Object.DontDestroyOnLoad(obj);
+        }
+
+        public Module GetModule(string id)
+        {
+            return _modules.FirstOrDefault(x => x.ID == id);
+        }
+
+        public bool Enabled(string id)
+        {
+            return _modules.Any(x => x.ID == id && x.Enabled);
+        }
+
+        public static IEnumerable<Module> Modules => _modules; 
     }
 }
--- Mod/Module.cs
+++ Mod/Module.cs
@@ -1,7 +1,84 @@
-﻿namespace Mod
+﻿using System;
+using Mod.Interface;
+using UnityEngine;
+
+namespace Mod
 {
-    public class Module
+    public abstract class Module : MonoBehaviour
     {
+        public abstract string ID { get; }
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract bool IsAbusive { get; }
+        public abstract bool HasGUI { get; }
+        public bool Enabled => _isEnabled;
         
+        private string PlayerPref => $"MOD|{ID}";
+        private bool _isEnabled;
+        
+        protected virtual void OnModuleEnable()
+        {
+        }
+
+        protected virtual void OnModuleUpdate()
+        {
+        }
+
+        protected virtual void OnModuleDisable()
+        {
+        }
+
+        public virtual Action<Rect> GetGUI()
+        {
+            return null;
+        }
+
+        public void Start()
+        {
+            _isEnabled = PlayerPrefs.GetString(PlayerPref, "False") == "True";
+            if (_isEnabled)
+                OnModuleEnable();
+        }
+
+        private void Update()
+        {
+            if (_isEnabled)
+                OnModuleUpdate();
+        }
+
+        public void Enable()
+        {
+            if (_isEnabled)
+                return;
+
+            _isEnabled = true;
+            PlayerPrefs.SetString(PlayerPref, _isEnabled.ToString());
+            OnStatusUpdate();
+        }
+
+        public void Disable()
+        {
+            if (!_isEnabled)
+                return;
+
+            _isEnabled = false;
+            PlayerPrefs.SetString(PlayerPref, _isEnabled.ToString());
+            OnStatusUpdate();
+        }
+
+        public void Toggle()
+        {
+            _isEnabled = !_isEnabled;
+            PlayerPrefs.SetString(PlayerPref, _isEnabled.ToString());
+            OnStatusUpdate();
+        }
+
+        private void OnStatusUpdate()
+        {
+            if (_isEnabled)
+                OnModuleEnable();
+            else
+                OnModuleDisable();
+        }
     }
 }
