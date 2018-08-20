8e3a5f5c9400e3532cfbab75681b0d63cb74cd6a
mHawk <italia195@outlook.it>
2018-08-21 01:03:42 +0200

    Added RichPresence in main menu

--- Mod/Shelter.cs
+++ Mod/Shelter.cs
@@ -6,7 +6,9 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using Mod.Animation;
+using Mod.Discord;
 using Mod.Interface;
+using Mod.Modules;
 using UnityEngine;
 using AnimationInfo = Mod.Animation.AnimationInfo;
 using Animator = Mod.Animation.Animator;
@@ -21,18 +23,18 @@ namespace Mod
         public static readonly Stopwatch Stopwatch = Stopwatch.StartNew();
         public static List<Profile> Profiles => _profileManager.ProfileFile.Profiles;
         public static Profile Profile => _profileManager.ProfileFile.SelectedProfile;
+        public static readonly long StartTime = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
         private static AnimationInfo _animation;
         private static InterfaceManager _interfaceManager;
         private static CommandManager _commandManager;
         private static AnimationManager _animationManager;
+        private static ModuleManager _moduleManager;
         private static ProfileManager _profileManager;
-//        private static DiscordRpc _discord;        
 
         public Shelter()
         {
             if (!Directory.Exists(ModDirectory))
                 Directory.CreateDirectory(ModDirectory);
-            gameObject.AddComponent<AnimationTest>();
 
             _animation = new AnimationInfo(AnimationType.Cycle, AnimationInfo.Rainbow);
         }
@@ -42,14 +44,19 @@ namespace Mod
             _interfaceManager = new InterfaceManager();
             _profileManager = new ProfileManager();
             _animationManager = new AnimationManager();
-//            _discord = new DiscordRpc();;
+            _moduleManager = new ModuleManager();
         }
 
         public void Update()
         {
-//            _discord.Update();
+            DiscordApi.RunCallbacks();
             if (Input.GetKeyDown(KeyCode.I) && Input.GetKey(KeyCode.LeftControl))
                 File.WriteAllLines($"GameObjects{Random.Range(0, 255)}.txt", FindObjectsOfType(typeof(GameObject)).OrderBy(x => x.GetInstanceID()).Select(x => $"{x.GetInstanceID()} - {x.name}").ToArray());
+        }
+
+        private void OnDisable()
+        {
+            DiscordApi.Shutdown();
         }
 
         public static void OnMainMenu()
@@ -57,19 +64,32 @@ namespace Mod
             _commandManager?.Dispose();
             _commandManager = null;
             _interfaceManager.DisableAll();
-//            _interfaceManager.Enable(nameof(InGameMenu));
             _interfaceManager.Enable(nameof(Background));
             _interfaceManager.Enable(nameof(Loading));
             _interfaceManager.Enable(nameof(MainMenu));
+            DiscordRpc.SendMainMenu();
         }
-
+        
+        
         public static void OnJoinedGame()
         {
             _commandManager = new CommandManager();
             _interfaceManager.DisableAll();
             _interfaceManager.Enable(nameof(Chat));
             _interfaceManager.Enable(nameof(Scoreboard));
-            _interfaceManager.Enable(nameof(GameInfo));
+            if (_moduleManager.Enabled(nameof(ModuleShowGameInfo)))
+                _interfaceManager.Enable(nameof(GameInfo));
+            DiscordApi.UpdatePresence(new DiscordApi.RichPresence
+            {
+                details = "In Lobby",
+                state = PhotonNetwork.Room.Name.RemoveColors().MaxChars(42),
+                joinSecret = PhotonNetwork.Room.FullName,
+                partyId = PhotonNetwork.Room.FullName,
+                partyMax = PhotonNetwork.Room.MaxPlayers,
+                partySize = Room.CurrentPlayers,
+                startTimestamp = StartTime,
+                largeImageKey = "city",
+            });
         }
 
         public static bool TryFind(string name, out GameObject go)
@@ -107,7 +127,8 @@ namespace Mod
         #endregion
 
         public static AnimationInfo Animation => _animation;
-        
+
+        public static ModuleManager ModuleManager => _moduleManager;
         public static ProfileManager ProfileManager => _profileManager;
         public static AnimationManager AnimationManager => _animationManager;
         public static InterfaceManager InterfaceManager => _interfaceManager;
