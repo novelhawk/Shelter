0fe00b45981b2abb20f1988e6e2ab4b2b77f8922
mHawk <italia195@outlook.it>
2018-08-21 01:01:50 +0200

    Added option to disable and enable GameInfo panel

--- Mod/Modules/ModuleShowGameInfo.cs
+++ Mod/Modules/ModuleShowGameInfo.cs
@@ -1,7 +1,24 @@
-﻿namespace Mod.Modules
+﻿using Mod.Interface;
+
+namespace Mod.Modules
 {
-    public class ModuleShowGameInfo
+    public class ModuleShowGameInfo : Module
     {
-        
+        public override string ID => nameof(ModuleRacingInterface);
+        public override string Name => "Show GameInfo";
+        public override string Description => "Shows the top right GameInfo panel.";
+        public override bool IsAbusive => false;
+        public override bool HasGUI => false; //TODO: Add custom infos selection
+
+        protected override void OnModuleEnable()
+        {
+            if (PhotonNetwork.inRoom)
+                Shelter.InterfaceManager.Enable(nameof(GameInfo));
+        }
+
+        protected override void OnModuleDisable()
+        {
+            Shelter.InterfaceManager.Disable(nameof(GameInfo));
+        }
     }
 }
