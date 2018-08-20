e020f925ae4dea94d3b6cecf2b61bfd3b5fa89c0
mHawk <italia195@outlook.it>
2018-08-21 01:01:03 +0200

    Added option to disable and enable Name Scaling

--- Mod/Modules/ModuleNameScaling.cs
+++ Mod/Modules/ModuleNameScaling.cs
@@ -1,7 +1,11 @@
 ﻿namespace Mod.Modules
 {
-    public class ModuleNameScaling
+    public class ModuleNameScaling : Module
     {
-        
+        public override string ID => nameof(ModuleNameScaling);
+        public override string Name => "Scale Names";
+        public override string Description => "Scales name size based on the distance";
+        public override bool IsAbusive => false;
+        public override bool HasGUI => false;
     }
 }
