4e2e975e9098af4fd200cccdf8d114f1cb695e2b
mHawk <italia195@outlook.it>
2018-08-21 20:00:46 +0200

    Improved SpectatorMovement

--- SpectatorMovement.cs
+++ SpectatorMovement.cs
@@ -6,74 +6,40 @@ public class SpectatorMovement : MonoBehaviour
 {
     public bool disable;
     public FengCustomInputs inputManager;
-    private float speed = 100f;
-
-    private void Start()
-    {
-        this.inputManager = GameObject.Find("InputManagerController").GetComponent<FengCustomInputs>();
-    }
 
     private void Update()
     {
-        if (!this.disable)
-        {
-            float num2;
-            float num3;
-            float speed = this.speed;
-            if (Shelter.InputManager.IsKeyPressed(InputAction.Jump))
-            {
-                speed *= 3f;
-            }
-            if (Shelter.InputManager.IsKeyPressed(InputAction.Forward))
-            {
-                num2 = 1f;
-            }
-            else if (Shelter.InputManager.IsKeyPressed(InputAction.Back))
-            {
-                num2 = -1f;
-            }
-            else
-            {
-                num2 = 0f;
-            }
-            if (Shelter.InputManager.IsKeyPressed(InputAction.Left))
-            {
-                num3 = -1f;
-            }
-            else if (Shelter.InputManager.IsKeyPressed(InputAction.Right))
-            {
-                num3 = 1f;
-            }
-            else
-            {
-                num3 = 0f;
-            }
-            Transform transform = this.transform;
-            if (num2 > 0f)
-            {
-                transform.position += this.transform.forward * speed * Time.deltaTime;
-            }
-            else if (num2 < 0f)
-            {
-                transform.position -= this.transform.forward * speed * Time.deltaTime;
-            }
-            if (num3 > 0f)
-            {
-                transform.position += this.transform.right * speed * Time.deltaTime;
-            }
-            else if (num3 < 0f)
-            {
-                transform.position -= this.transform.right * speed * Time.deltaTime;
-            }
-            if (Shelter.InputManager.IsKeyPressed(InputAction.LeftHook))
-            {
-                transform.position -= this.transform.up * speed * Time.deltaTime;
-            }
-            else if (Shelter.InputManager.IsKeyPressed(InputAction.RightHook))
-            {
-                transform.position += this.transform.up * speed * Time.deltaTime;
-            }
-        }
+        if (this.disable) 
+            return;
+
+        float speed = 100;
+        if (Shelter.InputManager.IsKeyPressed(InputAction.Jump))
+            speed *= 4f;
+        if (Shelter.InputManager.IsKeyPressed(InputAction.Dodge))
+            speed *= 0.3f;
+            
+        float x = 0;
+        if (Shelter.InputManager.IsKeyPressed(InputAction.Forward))
+            x = 1;
+        else if (Shelter.InputManager.IsKeyPressed(InputAction.Back))
+            x = -1;
+            
+        float y = 0;
+        if (Shelter.InputManager.IsKeyPressed(InputAction.Left))
+            y = -1f;
+        else if (Shelter.InputManager.IsKeyPressed(InputAction.Right))
+            y = 1f;
+            
+        if (x > 0f)      transform.position += transform.forward * speed * Time.deltaTime;
+        else if (x < 0f) transform.position -= transform.forward * speed * Time.deltaTime;
+        
+        if (y > 0f)      transform.position += transform.right * speed * Time.deltaTime;
+        else if (y < 0f) transform.position -= transform.right * speed * Time.deltaTime;
+        
+        if (Shelter.InputManager.IsKeyPressed(InputAction.LeftHook))
+            transform.position -= transform.up * speed * Time.deltaTime;
+        else if (Shelter.InputManager.IsKeyPressed(InputAction.RightHook))
+            transform.position += transform.up * speed * Time.deltaTime;
     }
 }
 
