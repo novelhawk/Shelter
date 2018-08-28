072c9d92268d1cd7ab84920c1253a93c0364d24d
mHawk <italia195@outlook.it>
2018-08-28 20:43:44 +0200

    Added custom JsonConverter for AnimationColors

--- Mod/Animation/ColorConverter.cs
+++ Mod/Animation/ColorConverter.cs
@@ -1,7 +1,44 @@
-﻿namespace Mod.Animation
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+namespace Mod.Animation
 {
-    public class ColorConverter
+    public class ColorConverter : JsonConverter<AnimationColor>
     {
+        private const string Pattern = @"(?:rgb\()?.*?(\d+)[,\s]+(\d+)[,\s]+(\d+)(?:[,\s]+(\d+))?.*?\)?|#?([A-Fa-f0-9]{6,8})";
         
+        public override void WriteJson(JsonWriter writer, AnimationColor value, JsonSerializer serializer)
+        {
+            writer.WriteValue($"#{value.ToHexFull()}");
+        }
+
+        public override AnimationColor ReadJson(JsonReader reader, Type objectType, AnimationColor existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value is string str)
+            {
+                Match match = Regex.Match(str, Pattern, RegexOptions.IgnoreCase);
+                if (!match.Success)
+                    throw new Exception("Cannot parse AnimationColor");
+
+                if (!string.IsNullOrEmpty(match.Groups[5].Value))
+                    return new AnimationColor(match.Groups[5].Value);
+                
+                if (byte.TryParse(match.Groups[1].Value, out byte r) &&
+                    byte.TryParse(match.Groups[2].Value, out byte g) &&
+                    byte.TryParse(match.Groups[3].Value, out byte b))
+                {
+                    if (!byte.TryParse(match.Groups[4].Value, out byte a))
+                        a = 255;
+                    
+                    return new AnimationColor(r, g, b, a);
+                }
+            }
+            return new AnimationColor(0, 0, 0, 0);
+        }
     }
 }
