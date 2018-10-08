df769264aae152e959f01a86c4f604cc3996c2ce
mHawk <italia195@outlook.it>
2018-10-08 20:19:45 +0200

    Made wind state change apply without the need to respawn.

--- Mod/Modules/ModuleWind.cs
+++ Mod/Modules/ModuleWind.cs
@@ -1,4 +1,7 @@
-﻿namespace Mod.Modules
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Mod.Modules
 {
     public class ModuleWind : Module
     {
@@ -7,5 +10,25 @@
         public override string Description => "Adds wind post-processing.";
         public override bool IsAbusive => false;
         public override bool HasGUI => false;
+
+        protected override void OnModuleEnable()
+        {
+            if (Player.Self.Properties.Alive == false || Player.Self.Hero == null)
+                return;
+                
+            var windRenderer = Player.Self.Hero.GetComponentsInChildren<Renderer>().FirstOrDefault(x => x.name.Contains("speed"));
+            if (windRenderer != null)
+                windRenderer.enabled = true;
+        }
+
+        protected override void OnModuleDisable()
+        {
+            if (Player.Self.Properties.Alive == false || Player.Self.Hero == null)
+                return;
+            
+            var windRenderer = Player.Self.Hero.GetComponentsInChildren<Renderer>().FirstOrDefault(x => x.name.Contains("speed"));
+            if (windRenderer != null)
+                windRenderer.enabled = false;
+        }
     }
 }
