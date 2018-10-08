7c5377b5cda5496e5b06d1a84159a340a340f0dd
mHawk <italia195@outlook.it>
2018-10-08 15:17:47 +0200

    Fix SpawnRates using double instead of float

--- Mod/GameSettings/SpawnRates.cs
+++ Mod/GameSettings/SpawnRates.cs
@@ -1,35 +1,34 @@
-﻿namespace Mod.GameSettings
+﻿using System;
+
+namespace Mod.GameSettings
 {
-    public struct SpawnRates
+    public struct SpawnRates : IEquatable<SpawnRates>
     {
-        public double Normal;
-        public double Aberrant;
-        public double Jumper;
-        public double Crawler;
-        public double Punk;
+        public static SpawnRates Zero = new SpawnRates();
+        
+        public readonly float Normal;
+        public readonly float Aberrant;
+        public readonly float Jumper;
+        public readonly float Crawler;
+        public readonly float Punk;
 
         public SpawnRates(string normal, string aberrant, string jumper, string crawler, string punk)
         {
-            if (!double.TryParse(normal, out Normal))
+            if (!float.TryParse(normal, out Normal))
                 Normal = 20;
 
-            if (!double.TryParse(aberrant, out Aberrant))
+            if (!float.TryParse(aberrant, out Aberrant))
                 Aberrant = 20;
 
-            if (!double.TryParse(jumper, out Jumper))
+            if (!float.TryParse(jumper, out Jumper))
                 Jumper = 20;
 
-            if (!double.TryParse(crawler, out Crawler))
+            if (!float.TryParse(crawler, out Crawler))
                 Crawler = 20;
 
-            if (!double.TryParse(punk, out Punk))
+            if (!float.TryParse(punk, out Punk))
                 Punk = 20;
             
-            VerifySums();
-        }
-
-        private void VerifySums()
-        {
             var sum = Normal + Aberrant + Jumper + Crawler + Punk; 
             if (sum == 100)
                 return;
@@ -48,6 +47,68 @@
             
             Punk /= sum;
             Punk *= 100;
+        }
+        
+        public SpawnRates(float normal, float aberrant, float jumper, float crawler, float punk)
+        {
+            Normal = normal;
+            Aberrant = aberrant;
+            Jumper = jumper;
+            Crawler = crawler;
+            Punk = punk;
+            
+            var sum = normal + aberrant + jumper + crawler + punk; 
+            if (sum == 100)
+                return;
+
+            Normal /= sum;
+            Normal *= 100;
+            
+            Aberrant /= sum;
+            Aberrant *= 100;
+            
+            Jumper /= sum;
+            Jumper *= 100;
+            
+            Crawler /= sum;
+            Crawler *= 100;
+            
+            Punk /= sum;
+            Punk *= 100;
+        }
+
+        public static bool operator ==(SpawnRates rates, SpawnRates other)
+        {
+            return rates.Equals(other);
+        }
+
+        public static bool operator !=(SpawnRates rates, SpawnRates other)
+        {
+            return !rates.Equals(other);
+        }
+
+        public bool Equals(SpawnRates other)
+        {
+            return Normal.Equals(other.Normal) && Aberrant.Equals(other.Aberrant) && Jumper.Equals(other.Jumper) && Crawler.Equals(other.Crawler) && Punk.Equals(other.Punk);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is SpawnRates rates && Equals(rates);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Normal.GetHashCode();
+                hashCode = (hashCode * 397) ^ Aberrant.GetHashCode();
+                hashCode = (hashCode * 397) ^ Jumper.GetHashCode();
+                hashCode = (hashCode * 397) ^ Crawler.GetHashCode();
+                hashCode = (hashCode * 397) ^ Punk.GetHashCode();
+                return hashCode;
+            }
         }
     }
 }
