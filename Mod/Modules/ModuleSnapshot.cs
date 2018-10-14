0a152a1b1671a8a79b896831826b8d4cf766ff00
mHawk <italia195@outlook.it>
2018-10-14 12:40:54 +0200

    Added Snapshot Module

---
+++ Mod/Modules/ModuleSnapshot.cs
@@ -0,0 +1,10 @@
+﻿namespace Mod.Modules
+{
+    public class ModuleSnapshot : Module // TODO: Add minimum damage required
+    {
+        public override string Name => "Kill Screenshot";
+        public override string Description => "Takes a screenshot on titan kill.";
+        public override bool IsAbusive => false;
+        public override bool HasGUI => false;
+    }
+}
