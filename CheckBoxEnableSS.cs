53e821b4fda02284d0eb048d3b5c07e1cf64480a
mHawk <italia195@outlook.it>
2018-10-14 12:41:09 +0200

    Removed UI element

--- CheckBoxEnableSS.cs
+++ CheckBoxEnableSS.cs
@@ -2,42 +2,11 @@ using UnityEngine;
 
 public class CheckBoxEnableSS : MonoBehaviour
 {
-    private bool init;
+//    private bool init; (Unity field)
 
-    private void OnActivate(bool yes)
+    private void Awake()
     {
-        if (this.init)
-        {
-            if (yes)
-            {
-                PlayerPrefs.SetInt("EnableSS", 1);
-            }
-            else
-            {
-                PlayerPrefs.SetInt("EnableSS", 0);
-            }
-        }
-    }
-
-    private void Start()
-    {
-        this.init = true;
-        if (PlayerPrefs.HasKey("EnableSS"))
-        {
-            if (PlayerPrefs.GetInt("EnableSS") == 1)
-            {
-                GetComponent<UICheckbox>().isChecked = true;
-            }
-            else
-            {
-                GetComponent<UICheckbox>().isChecked = false;
-            }
-        }
-        else
-        {
-            GetComponent<UICheckbox>().isChecked = true;
-            PlayerPrefs.SetInt("EnableSS", 1);
-        }
+        Destroy(this);
     }
 }
 
