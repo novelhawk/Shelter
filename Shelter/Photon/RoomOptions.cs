39acfb0fbdee342d0fac2e466f00003563d3c720
mHawk <italia195@outlook.it>
2018-10-20 22:43:25 +0200

    Fixed cast exception on creating room.

--- Shelter/Photon/RoomOptions.cs
+++ Shelter/Photon/RoomOptions.cs
@@ -8,37 +8,25 @@ namespace Photon
         public bool? RemovedFromList => (bool?) this[GamePropertyKey.Removed];
         public bool? DoAutoCleanup => (bool?) this[GamePropertyKey.CleanupCacheOnLeave];
         public int? CurrentPlayers => (byte?) this[GamePropertyKey.PlayerCount];
-        public int? PlayerTTL => (int?) this[ParameterCode.PlayerTTL];
-        public int? RoomTTL => (int?) this[ParameterCode.EmptyRoomTTL];
+        public int? PlayerTTL => (int?) this[GamePropertyKey.PlayerTTL];
+        public int? RoomTTL => (int?) this[GamePropertyKey.EmptyRoomTTL];
     
         public int? MaxPlayers
         {
-            get => (byte?) this[GamePropertyKey.MaxPlayers];
+            get => this[GamePropertyKey.MaxPlayers] as byte?;
             set => this[GamePropertyKey.MaxPlayers] = value;
         }
-    
+
         public bool? IsVisible
         {
-            get => (bool?) this[GamePropertyKey.IsVisible];
+            get => this[GamePropertyKey.IsVisible] as bool?;
             set => this[GamePropertyKey.IsVisible] = value;
         }
     
         public bool? IsOpen
         {
-            get => (bool?) this[GamePropertyKey.IsOpen];
+            get => this[GamePropertyKey.IsOpen] as bool?;
             set => this[GamePropertyKey.IsOpen] = value;
-        }
-
-        public RoomOptions()
-        {}
-
-        public RoomOptions(Hashtable hash)
-        {
-            if (hash == null)
-                return;
-        
-            foreach (var entry in hash)
-                this[entry.Key] = entry.Value;
         }
     }
 }
