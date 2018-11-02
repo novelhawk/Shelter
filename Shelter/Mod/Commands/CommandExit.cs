0cbfde658284899de71afc0d9bd4298d7efa5824
mHawk <italia195@outlook.it>
2018-11-02 13:18:34 +0100

    Added Exit command.

---
+++ Shelter/Mod/Commands/CommandExit.cs
@@ -0,0 +1,19 @@
+﻿using Photon;
+using UnityEngine;
+
+namespace Mod.Commands
+{
+    public class CommandExit : Command
+    {
+        public override string CommandName => "exit";
+        public override string[] Aliases => new[] {"quit"};
+
+        public override void Execute(string[] args)
+        {
+            if (args.Length > 0)
+                Application.Quit();
+            
+            PhotonNetwork.Disconnect();
+        }
+    }
+}
