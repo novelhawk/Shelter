9497ab59fad14889092f0aa64d1516ff53304025
mHawk <italia195@outlook.it>
2018-11-03 18:44:11 +0100

    Removed duplicate file check.

--- Shelter/Mod/Logging/FileLogger.cs
+++ Shelter/Mod/Logging/FileLogger.cs
@@ -8,7 +8,7 @@ namespace Mod.Logging
     public class FileLogger : ILogger
     {
         public const string Latest = "latest.log";
-        private const string Old = "{0:00}-{1:00}-{2} {3:00}-{4:00}-{5:00}.log";
+        private const string FormerLogFileName = "{0:00}-{1:00}-{2} {3:00}-{4:00}-{5:00}.log";
         
         private readonly string _path = Application.dataPath + "/Logs/";
         private FileStream _fileStream;
@@ -19,24 +19,17 @@ namespace Mod.Logging
                 Directory.CreateDirectory(_path);
 
             if (File.Exists(_path + Latest))
-                MoveOldLogs();
+                DisposeFormerLog();
                 
             SafeOpenFileStream(_path + Latest);
         }
         
-        private void MoveOldLogs()
+        private void DisposeFormerLog()
         {
             var info = new FileInfo(_path + Latest);
-            StringBuilder builder = new StringBuilder(Old + 4);
-            for (int i = 0; i < 1 || File.Exists(builder.ToString()); i++)
-            {
-                builder.Length = 0;
-                builder.Append(DateFormat(Old, info.CreationTime));
-                if (i > 0)
-                    builder.AppendFormat(" ({0})", i);
-            }
-            
-            File.Move(_path + Latest, _path + builder);
+
+            var fileName = DateFormat(FormerLogFileName, info.CreationTime);
+            File.Move(_path + Latest, _path + fileName);
         }
 
         private static string DateFormat(string line, DateTime time)
