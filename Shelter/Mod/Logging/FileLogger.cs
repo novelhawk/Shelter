f2a91450d82283956edac963b3f570956b83f0be
mHawk <italia195@outlook.it>
2018-11-04 14:19:45 +0100

    Added option to save old logs.

--- Shelter/Mod/Logging/FileLogger.cs
+++ Shelter/Mod/Logging/FileLogger.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using JetBrains.Annotations;
+using Mod.Managers;
+using Mod.Modules;
 using UnityEngine;
 
 namespace Mod.Logging
@@ -9,6 +12,7 @@ namespace Mod.Logging
     {
         public const string Latest = "latest.log";
         private const string FormerLogFileName = "{0:00}-{1:00}-{2} {3:00}-{4:00}-{5:00}.log";
+        private const string Header = "# Created {0:00}/{1:00}/{2} at {3:00}:{4:00}:{5:00}";
         
         private readonly string _path = Application.dataPath + "/Logs/";
         private FileStream _fileStream;
@@ -18,18 +22,28 @@ namespace Mod.Logging
             if (!Directory.Exists(_path))
                 Directory.CreateDirectory(_path);
 
-            if (File.Exists(_path + Latest))
-                DisposeFormerLog();
-                
+            DisposeFormerLog();
             SafeOpenFileStream(_path + Latest);
         }
         
         private void DisposeFormerLog()
         {
-            var info = new FileInfo(_path + Latest);
+            if (!File.Exists(_path + Latest))
+                return;
 
-            var fileName = DateFormat(FormerLogFileName, info.CreationTime);
-            File.Move(_path + Latest, _path + fileName);
+            if (ModuleManager.Enabled(nameof(ModuleSaveLogs)))
+            {
+                // Rename old logs
+                var info = new FileInfo(_path + Latest);
+
+                var fileName = DateFormat(FormerLogFileName, info.CreationTime);
+                File.Move(_path + Latest, _path + fileName);
+            }
+            else
+            {
+                // Delete old logs
+                File.Delete(_path + Latest);
+            }
         }
 
         private static string DateFormat(string line, DateTime time)
@@ -45,15 +59,15 @@ namespace Mod.Logging
             {
                 _fileStream = File.Open(path, FileMode.Create, FileAccess.Write, FileShare.Read);
             }
-            catch (Exception e)
+            catch
             {
-                Debug.Log("Couldn't create the FileLogger. Client will now close.");
-                Debug.LogException(e);
+                // Cannot log as unity's log are bounded to Shelter's
+                // TODO: Redirect unity logs only after successful initialization
                 Application.Quit();
             }
             
             Log($"# Shelter Logs{Environment.NewLine}");
-            Log("{0}{1}{1}", DateFormat("# Created {0:00}/{1:00}/{2} at {3:00}:{4:00}:{5:00}", DateTime.Now), Environment.NewLine);
+            Log("{0}{1}{1}", DateFormat(Header, DateTime.Now), Environment.NewLine);
         }
 
         public void Log(string line, params object[] args) => Log(string.Format(line, args));
---
+++ Shelter/Mod/Modules/ModuleSaveLogs.cs
@@ -0,0 +1,10 @@
+﻿namespace Mod.Modules
+{
+    public class ModuleSaveLogs : Module
+    {
+        public override string Name => "Save Logs";
+        public override string Description => "Keeps a copy of former logs in the /Logs/ directory.";
+        public override bool IsAbusive => false;
+        public override bool HasGUI => false;
+    }
+}
