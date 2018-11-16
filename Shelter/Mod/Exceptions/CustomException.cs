69a118de08db1f515a2ee9eec7ba813b0851008b
mHawk <italia195@outlook.it>
2018-11-16 17:37:59 +0100

    Made methods protected

--- Shelter/Mod/Exceptions/CustomException.cs
+++ Shelter/Mod/Exceptions/CustomException.cs
@@ -6,17 +6,15 @@ namespace Mod.Exceptions
     [Serializable]
     public class CustomException : Exception
     {
-        public CustomException()
-        {
-            
-        }
+        protected CustomException()
+        {}
 
-        public CustomException(string message)
+        protected CustomException(string message)
         {
             Notify.New(message, 3000);
         }
 
-        public CustomException(string message, float height)
+        protected CustomException(string message, float height)
         {
             Notify.New("<color=#ff0000>Error</color>", message, 3500, height);
         }
