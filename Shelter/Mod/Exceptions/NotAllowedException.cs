faca713200d224f6d2f94403c47492da0d502b0b
mHawk <italia195@outlook.it>
2018-11-16 17:37:28 +0100

    Added Toast notification for NotAllowedException

--- Shelter/Mod/Exceptions/NotAllowedException.cs
+++ Shelter/Mod/Exceptions/NotAllowedException.cs
@@ -7,15 +7,15 @@ namespace Mod.Exceptions
     [Serializable]
     public class NotAllowedException : CustomException
     {
-        public NotAllowedException(byte eventId, Player sender)
+        public NotAllowedException(byte eventId, Player sender) : base(string.Format("Blocked Event({1}) from {0}", sender.Properties.HexName, (PhotonEvent) eventId))
         {
-            Shelter.LogBoth("{0} from {1} on calling Event({2})", LogType.Warning, nameof(NotAllowedException), sender, (PhotonEvent) eventId);
+            Shelter.LogBoth(nameof(NotAllowedException) + " from {0} on calling Event({1})", LogType.Warning, sender, (PhotonEvent) eventId);
             sender.Ignore();
         }
 
-        public NotAllowedException(string rpc, PhotonMessageInfo info, bool ignore = true)
+        public NotAllowedException(string rpc, PhotonMessageInfo info, bool ignore = true) : base(string.Format("Blocked RPC({1}) from {0}", info.sender.Properties.HexName, rpc))
         {
-            Shelter.LogBoth("{0} from {1} on calling RPC({2})", LogType.Warning, nameof(NotAllowedException), info.sender, rpc);
+            Shelter.LogBoth(nameof(NotAllowedException) + " from {0} on calling RPC({1})", LogType.Warning, info.sender, rpc);
             if (ignore)
                 info.sender.Ignore();
         }
